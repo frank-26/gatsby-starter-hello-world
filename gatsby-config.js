81922e5340070a2ee561e6811252983144442553
frank-26 <1393273899@qq.com>
2020-03-13 18:17:39 +0800

    mvp

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,18 @@
 module.exports = {
+  siteMetadata: {
+    title: `Title from siteMetadata aha`,
+    description: `A simple description about pandas eating lots...`,
+    author: `gatsbyjs`,
+  },
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+    // `gatsby-transformer-remark`, // BUG
     {
       resolve: `gatsby-plugin-typography`,
       options: {
